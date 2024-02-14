c10a36de41c97ad10b3d09bf6657642f08b56461
w1949506 <w1949506@E-5112-05>
2024-02-14 15:59:50 +0000

    Add project files.

---
+++ Models/Organisation.cs
@@ -0,0 +1,11 @@
+﻿namespace CourseWork.Models
+{
+    public class Organisation
+    {
+        public int Id { get; set; }
+        public string? Deadline { get; set; }
+        public string? Description { get; set; }
+        public int? Duration { get; set; }
+        public Department? Department { get; set; }
+    }
+}
---
+++ Models/Skill.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseWork.Models
+{
+    public class Skill
+    {
+        public int? Id { get; set; }
+        public string? Type { get; set; }
+        public string? Level { get; set; }
+        public string? Description { get; set; }
+    }
+}
